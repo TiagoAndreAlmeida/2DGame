92be5c27417cc3f26d81dca6b93d7b7d32a10d3d
TiagoAndreAlmeida <tiagoeap@gmail.com>
2017-01-19 15:13:45 -0300

    Testando movimentação

---
+++ 2DGame/Assets/scripts/moveTest.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveTest : MonoBehaviour {
+    public Rigidbody2D rb;
+    public float forca;
+    // Use this for initialization
+    void FixedUpdate() {
+        float input = Input.GetAxis("Horizontal");
+        if(Mathf.Abs(rb.velocity.x) <= 4f)
+            rb.AddForce(Vector2.right * input * forca);
+    }
+}
