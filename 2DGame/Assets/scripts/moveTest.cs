0ac2dbabec38b2463bf2597e6e8de03ca6cc8e12
TiagoAndreAlmeida <tiagoeap@gmail.com>
2017-01-20 17:36:54 -0300

    Testando movimentação (update-1)
    grab adicionado

--- 2DGame/Assets/scripts/moveTest.cs
+++ 2DGame/Assets/scripts/moveTest.cs
@@ -4,11 +4,34 @@ using UnityEngine;
 
 public class moveTest : MonoBehaviour {
     public Rigidbody2D rb;
-    public float forca;
+    public bool grab;
+    public Transform pos;
+    public float power;
     // Use this for initialization
+   /* void Update() {
+            //print("TESTE");
+            Vector2.MoveTowards(this.transform.position, pos.position, 1f);
+    }*/
     void FixedUpdate() {
         float input = Input.GetAxis("Horizontal");
-        if(Mathf.Abs(rb.velocity.x) <= 4f)
-            rb.AddForce(Vector2.right * input * forca);
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            rb.AddForce(Vector2.up * 5, ForceMode2D.Impulse);
+        }
+
+        if (Mathf.Abs(rb.velocity.x) <= 4f)
+            rb.AddForce(Vector2.right * input * power);
+    }
+
+    void OnTriggerEnter2D(Collider2D hit) {
+        rb.Sleep();
+        rb.gravityScale = 0;
+        grab = true;
+    }
+
+    void OnTriggerExit2D(Collider2D hit) {
+        rb.WakeUp();
+        rb.gravityScale = 1;
+        grab = false;
     }
 }
